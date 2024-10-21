0e69a8805d065e21b6676b7cfeed6043c839971c
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2024-10-21 16:01:32 +0100

    hellowordPractise in python

---
+++ Python/Week1/Day1/Practice/helloWorld.py
@@ -0,0 +1,16 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Jabrane"
+print("hello "+ name +" !")
+print("hello "+ name +"!")
+# 3. print "Hello 42!" with the number in a variable
+number =42
+print("hello ", number ," !")
+print("hello "+ str(number) +"!")
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to eat {0} and {1} ".format(fave_food1,fave_food2) ) # with .format()
+# print("") # with an f string
+print(f"i love to eat lot of  {fave_food1} and {fave_food2} ")
