adfd38968b45b9ad635ada6dcbe3428f39a5b478
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2024-10-26 16:56:52 +0100

    greatnumber

--- Python/Week1/Day5/Practice/Great Number Game/server.py
+++ Python/Week1/Day5/Practice/Great Number Game/server.py
@@ -1,16 +1,26 @@
-from flask import Flask,render_template  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def level1():
+from flask import Flask, render_template, session, redirect,request
+import random
 
-    return "hello great number!"  # Return the string 'Hello World!' as a response
+app = Flask(__name__)
 
+app.secret_key="Benny bob wuz heer."
 
+@app.route('/')
+def index():
+    if "num" not in session:
+        session['num'] = random.randint(1,100)
 
+    return render_template("index.html")
 
+@app.route('/guess',methods=['POST'])
+def guess():
+    session['guess'] = int(request.form['guess'])
+    return redirect('/')
 
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
-
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
-
+if __name__=="__main__":
+    app.run(debug=True)
