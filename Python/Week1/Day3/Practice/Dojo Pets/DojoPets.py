e64e5164dabe7c1735ec3a6da7eae56f7a530182
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2024-10-26 14:35:38 +0100

    moving folder and doing DojoPets

---
+++ Python/Week1/Day3/Practice/Dojo Pets/DojoPets.py
@@ -0,0 +1,52 @@
+class ninja():
+    def __init__(self,first_name, last_name, pet, treats, pet_food):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.pet=pet
+        self.treats=treats
+        self.pet_food=pet_food
+    
+    
+    def walk(self):
+
+        return self
+    
+
+    def feed(self):
+
+        return self
+    
+
+    def bathe(self):
+
+        return self     
+
+    
+
+
+
+
+class pet():
+    def __init__(self, name, type, tricks, health, energy):
+        self.name=name
+        self.type=type
+        self.tricks=tricks
+        self.health=health
+        self.energy=energy
+
+    def sleep(self):
+
+        return self     
+
+    def eat(self):
+
+        return self     
+
+    def play(self):
+
+        return self 
+
+
+    def noise(self):
+
+        return self     
