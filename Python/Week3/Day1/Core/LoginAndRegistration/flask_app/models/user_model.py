f07b9c5d8a2ec00581352118c2ffd25ddd308c12
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2024-11-06 09:14:33 +0100

    Login And Registration Core

---
+++ Python/Week3/Day1/Core/LoginAndRegistration/flask_app/models/user_model.py
@@ -0,0 +1,58 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DB
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+class User:
+    def __init__(self,data):
+        self.id=data['id']
+        self.first_name=data['first_name']
+        self.last_name=data['last_name']
+        self.email=data['email']
+        self.password=data['password']
+        self.created_at=data['created_at']
+        self.updated_at=data['updated_at']
+
+
+    @classmethod
+    def register(cls,data):
+        query=""" insert into users (first_name, last_name, email, password)
+                values(%(first_name)s,%(last_name)s,%(email)s,%(password)s);
+                """  
+        return connectToMySQL(DB).query_db(query,data)
+    
+
+    @classmethod
+    def get_by_email(cls,data):
+        query="""
+                select * from users where email=%(email)s; 
+                """
+        result=connectToMySQL(DB).query_db(query,data)
+        if result:
+            return cls(result[0])
+        return False
+
+
+
+    @staticmethod
+    def validate(data):
+        is_valid=True
+        if len(data['first_name'])<2:
+            flash("First name must be longer than 2 characters","first_name")
+            is_valid=False
+        if len(data['last_name'])<2:
+            flash("Last name must be longer than 2 characters","last_name")
+            is_valid=False 
+        if not EMAIL_REGEX.match(data['email']):
+            flash("Email not valid !","email")
+            is_valid=False
+        elif User.get_by_email({"email":data["email"]}):
+            flash("email already exist","email")
+            is_valid=False
+        if len(data["password"])<7 :
+            flash("password need  to be at least 7 characters","password")
+            is_valid=False
+        elif data["password"] != data["confirm_password"]:
+            flash("passwords does not match","confirm_password")
+            is_valid=False
+        return is_valid        
